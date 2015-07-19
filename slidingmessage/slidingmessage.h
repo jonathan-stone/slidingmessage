8390fcc1e01d9ab102f64ca80e8c5cfe4f7e2094
Jonathan Stone <jjstone@yahoo.com>
2015-07-18 18:21:54 -0700

    Initial Commit

---
+++ slidingmessage/slidingmessage.h
@@ -0,0 +1,19 @@
+//
+//  slidingmessage.h
+//  slidingmessage
+//
+//  Created by Jonathan Stone on 7/18/15.
+//  Copyright (c) 2015 CompassionApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for slidingmessage.
+FOUNDATION_EXPORT double slidingmessageVersionNumber;
+
+//! Project version string for slidingmessage.
+FOUNDATION_EXPORT const unsigned char slidingmessageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <slidingmessage/PublicHeader.h>
+
+
